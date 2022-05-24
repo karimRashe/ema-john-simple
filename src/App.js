43a87e8d76195fb9f7f64209168b14ed54a3063f
karimRashe <rs.karim.4@gmail.com>
2022-05-25 02:06:46 +0600

    rahed

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import './App.css';
 import About from './components/About/About';
 import Header from './components/Header/Header';
 import Inventory from './components/Inventory/Inventory';
+import Login from './components/Login/Login';
 import Orders from './components/Orders/Orders';
+import REquirAuth from './components/RequireAuth/REquirAuth';
+import Shipment from './components/Shipment/Shipment';
 import Shop from './components/Shop/Shop';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
   return (
@@ -15,8 +19,20 @@ function App() {
         <Route path='/' element={<Shop></Shop>}></Route>
         <Route path='/shop' element={<Shop></Shop>}></Route>
         <Route path='/orders' element={<Orders></Orders>}></Route>
-        <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+        <Route path='/inventory' element={
+          <REquirAuth>
+            <Inventory></Inventory>
+          </REquirAuth>
+        }></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
+        <Route path='/shipment' element={
+          <REquirAuth>
+            <Shipment></Shipment>
+          </REquirAuth>
+        }></Route>
+
       </Routes>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,20 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 import logo from '../../images/Logo.svg';
 import './Header.css';
 
 const Header = () => {
+
+    const [user] = useAuthState(auth)
+
+    const handleSignOut = () => {
+        signOut(auth);
+    }
     return (
+
         <nav className='header'>
             <img src={logo} alt="" />
             <div>
@@ -12,6 +22,16 @@ const Header = () => {
                 <Link to="/orders">Orders</Link>
                 <Link to="/inventory">Inventory</Link>
                 <Link to="/about">About</Link>
+                {
+                    user ?
+                        <button onClick={handleSignOut}>Sign Out</button>
+                        :
+
+                        <Link to="/login">Login</Link>
+
+
+                }
+
             </div>
         </nav>
     );
